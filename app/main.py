faf24b9209ac73160199c6d344483cf547cb1f2f
callendev20 <callendev20@gmail.com>
2025-05-26 21:09:47 +0900

    :white_check_mark: feat: add api_router to main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from app.util.logging_utils import setup_logging
+from app.api.router import api_router
 
 setup_logging(level="info")
 
 app = FastAPI(title="CMS Storage Server API")
 
 
-
+app.include_router(api_router, prefix="/api")
 
 @app.get("/")
 def root():
