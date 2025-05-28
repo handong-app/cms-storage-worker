e8d0cba78a0beff00adcb674ee6f0ad5e763b0cb
callendev20 <callendev20@gmail.com>
2025-05-28 21:26:02 +0900

    :white_check_mark: feat: add env variables to config - Celery, RabbitMQ, Redis

--- src/core/config.py
+++ src/core/config.py
@@ -12,8 +12,28 @@ class EnvVariables:
     SIGNATURE_VERSION = os.getenv('SIGNATURE_VERSION')
 
     # Celery + RabbitMQ
-    RABBITMQ_URL: str = "amqp://guest:guest@rabbitmq:5672//"
-    CELERY_RESULT_BACKEND: str = "rpc://"
+    RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq")
+    RABBITMQ_URL: str = os.getenv('RABBITMQ_URL', "amqp://guest:guest@rabbitmq:5672//")
+    CELERY_RESULT_BACKEND: str = os.getenv('CELERY_RESULT_BACKEND', "rpc://")
+    TRANSCODE_STATUS_QUEUE = os.getenv('TRANSCODE_STATUS_QUEUE', "transcode.status")
+    TRANSCODE_REQUEST_QUEUE = os.getenv('TRANSCODE_REQUEST_QUEUE', "transcode.request")
+
+    # Redis
+    REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+    REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+    REDIS_CHANNEL = os.getenv("REDIS_CHANNEL", "transcode.status")
+
+    # For test
+    # 테스트용 RabbitMQ 연결 설정
+    TEST_RABBITMQ_HOST = os.getenv("TEST_RABBITMQ_HOST", "rabbitmq")
+    TEST_RABBITMQ_URL = os.getenv("TEST_RABBITMQ_URL", "amqp://guest:guest@localhost:5672//")
+    TEST_TRANSCODE_REQUEST_QUEUE = os.getenv("TEST_TRANSCODE_REQUEST_QUEUE", "transcode.request")
+    TEST_TRANSCODE_STATUS_QUEUE = os.getenv("TEST_TRANSCODE_STATUS_QUEUE", "transcode.status")
+
+    # 테스트용 Redis 설정
+    TEST_REDIS_HOST = os.getenv("TEST_REDIS_HOST", "redis")
+    TEST_REDIS_PORT = os.getenv("TEST_REDIS_PORT", 6379)
+    TEST_REDIS_CHANNEL = os.getenv("TEST_REDIS_CHANNEL", "transcode.status")
 
     @staticmethod
     def get_routes_by_prefix(prefix):
