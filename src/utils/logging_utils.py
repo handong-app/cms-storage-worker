94fd35ed8874e278fc41a9b51ea61c9911ae3d85
callendev20 <callendev20@gmail.com>
2025-05-28 21:24:57 +0900

    :mute: chore: prevent duplicate log propagation by setting logger.propagate = False in custom logger setup

--- src/utils/logging_utils.py
+++ src/utils/logging_utils.py
@@ -11,6 +11,13 @@ LOG_LEVELS = {
 
 def setup_logger(level: str = "info", name: str = __name__) -> logging.Logger:
     logger = logging.getLogger(name)
+    """
+    logger.propagate = True (기본값) 이면 로그 메시지가 부모 로거에게 전달된다.
+    Celery 나 Django 같은 프레임워크는 기본적으로 로깅세팅이 되어있으므로,
+    결과적으로 내가 설정한 로거에서 출력 (1) 부모로거(프레임워크 기본로거)에서 출력 (2) 된다.
+    그래서 False 로 해두면 부모로거에 전달되지 않으므로 1번만 로그를 출력할 수 있게 된다. 
+    """
+    logger.propagate = False
     log_level = LOG_LEVELS.get(level.lower(), logging.INFO)
     logger.setLevel(log_level)
 
