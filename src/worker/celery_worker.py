eb48f3ae87e61d30f524ae1da07abc19d07c3fca
callein <callendev20@gmail.com>
2025-06-07 22:44:46 +0900

    :recycle: refactor: change transcode_video_task queue to EnvVariables.CELERY_TRANSCODE_QUEUE

--- src/worker/celery_worker.py
+++ src/worker/celery_worker.py
@@ -1,12 +1,16 @@
 from celery import Celery
 from src.core.config import EnvVariables
 
-# Celery 인스턴스 생성
-celery_app = Celery(
-    "worker",
-    broker=EnvVariables.RABBITMQ_URL,
-    backend=EnvVariables.CELERY_RESULT_BACKEND,
-)
+
+broker_url = EnvVariables.EXTERN_RABBITMQ_URL
+result_backend = EnvVariables.CELERY_RESULT_BACKEND
+
+celery_app = Celery("worker", broker=broker_url, backend=result_backend)
+
+celery_app.conf.task_routes = {
+    'transcode_video_task': {'queue': EnvVariables.CELERY_TRANSCODE_QUEUE}
+}
+
 
 from src.tasks import transcode_video_task
 
